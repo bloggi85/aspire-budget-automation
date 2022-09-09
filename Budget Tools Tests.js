b0db9e66e40457c2a66e14cfc6a79a41cae901d4
Laurent Block <laurentblock1@gmail.com>
2022-09-09 16:59:07 +0100

    first implementation

---
+++ Budget Tools Tests.js
@@ -0,0 +1,324 @@
+
+var testRange;
+var testRangeFull;
+const testRangeStartRow = 2;
+const testRangeStartColumn = 2;
+const testRangeValues =     [
+  [ "B2", "C2", "D2","E2", "F2" ],
+  [ "B3", "C3", "D3","E3", "F3" ],
+  [ "B4", "C4", "D4","E4", "F4" ],
+  [ "B5", "C5", "D5","E5", "F5" ],
+  [ "B6", "C6", "D6","E6", "F6" ],
+  [ "B7", "C7", "D7","E7", "F7" ],
+  [ "B8", "C8", "D8","E8", "F8" ],
+  [ "B9", "C9", "D9","E9", "F9" ],
+  [ "B10", "C10", "D10","E10", "F10" ],
+  [ "B11", "C11", "D11","E11", "F11" ],
+  [ "B12", "C12", "D12","E12", "F12" ],
+  [ "B13", "C13", "D13","E13", "F13" ],
+  [ "B14", "C14", "D14","E14", "F14" ],
+  [ "B15", "C15", "D15","E15", "F15" ],
+  [ "B16", "C16", "D16","E16", "F16" ],
+  [ "B17", "C17", "D17","E17", "F17" ],
+  [ "B18", "C18", "D18","E18", "F18" ],
+  [ "B19", "C19", "D19","E19", "F19" ],
+  [ "B20", "C20", "D20","E20", "F20" ],
+  [ "B21", "C21", "D21","E21", "F21" ],
+  [ "B22", "C22", "D22","E22", "F22" ],
+  [ "B23", "C23", "D23","E23", "F23" ],
+  [ "B24", "C24", "D24","E24", "F24" ],
+  [ "B25", "C25", "D25","E25", "F25" ],
+  [ "B26", "C26", "D26","E26", "F26" ],
+];
+
+
+function testAll(){
+  testAddCategoryAssertAddedSuccessfully();
+  testAddCategoryAssertRangeFull();
+  testGetLastRowIndexInDataRange();
+  testDeleteRowFromRangeByShiftingRowsUpAssertDeleteSuccess();
+  testDeleteRowFromRangeByShiftingRowsUpAssertLastRowAndDeleteSuccess();
+  testFindCategoryAssertFound();
+}
+
+
+function setUp_() {
+  const rowCount = testRangeValues.length ;
+  const columnCount = testRangeValues[0].length;
+  var sheet = SpreadsheetApp.getActive().getSheetByName("__test_sheet");
+  if (sheet != null ) {
+    SpreadsheetApp.getActive().deleteSheet( sheet );
+  }
+  sheet = SpreadsheetApp.getActive().insertSheet("__test_sheet");
+
+  sheet.getRange(testRangeStartRow,testRangeStartColumn,rowCount,columnCount).setValues( testRangeValues ); 
+  testRange = sheet.getRange( testRangeStartRow,testRangeStartColumn,rowCount+100,columnCount);
+  testRangeFull = sheet.getRange( testRangeStartRow,testRangeStartColumn,rowCount,columnCount);
+}
+
+function assertEquals_(message, expected, actual) {
+  if (actual !== expected) {
+    Logger.log( message + " Actual value '" + actual + "' does not match expected value '" + expected + "'");
+  }
+}
+function assert_(message, assert) {
+  if (assert !== true) {
+    Logger.log(message);
+  }
+}
+function tearDown_(){
+  if (testRange != null) {
+    SpreadsheetApp.getActive().deleteSheet( testRange.getSheet());
+  }
+}
+
+
+
+
+function testAddCategoryAssertAddedSuccessfully() {
+
+  setUp_();
+
+
+  var expected =  [
+    [ "B2", "C2", "D2","E2", "F2" ],
+    [ "B3", "C3", "D3","E3", "F3" ],
+    [ "B4", "C4", "D4","E4", "F4" ],
+    [ "B5", "C5", "D5","E5", "F5" ],
+    ["a", "b", "c", "d", "e"],
+    [ "B6", "C6", "D6","E6", "F6" ],
+    [ "B7", "C7", "D7","E7", "F7" ],
+    [ "B8", "C8", "D8","E8", "F8" ],
+    [ "B9", "C9", "D9","E9", "F9" ],
+    [ "B10", "C10", "D10","E10", "F10" ],
+    [ "B11", "C11", "D11","E11", "F11" ],
+    [ "B12", "C12", "D12","E12", "F12" ],
+    [ "B13", "C13", "D13","E13", "F13" ],
+    [ "B14", "C14", "D14","E14", "F14" ],
+    [ "B15", "C15", "D15","E15", "F15" ],
+    [ "B16", "C16", "D16","E16", "F16" ],
+    [ "B17", "C17", "D17","E17", "F17" ],
+    [ "B18", "C18", "D18","E18", "F18" ],
+    [ "B19", "C19", "D19","E19", "F19" ],
+    [ "B20", "C20", "D20","E20", "F20" ],
+    [ "B21", "C21", "D21","E21", "F21" ],
+    [ "B22", "C22", "D22","E22", "F22" ],
+    [ "B23", "C23", "D23","E23", "F23" ],
+    [ "B24", "C24", "D24","E24", "F24" ],
+    [ "B25", "C25", "D25","E25", "F25" ],
+    [ "B26", "C26", "D26","E26", "F26" ],
+  ];
+
+  // Add after the row we found. Adding + 1
+  addDataIntoRangeAtRowNumber_(testRange, 6, [["a", "b", "c", "d", "e"]]);
+
+  var actualValues = testRange.getValues();
+  var consecutiveFilledRows = 0;
+  for(var i = 0; i<actualValues.length;i++) {
+    if (actualValues[i][0] != "" || actualValues[i][1] != "" || actualValues[i][2] != "" || actualValues[i][3] != "") {
+      consecutiveFilledRows++;
+    }
+    for (var j = 0;j<actualValues[i].length;j++){ 
+      // Any cell values we cannot predict should be blank
+      if ( i >= expected.length ) {
+        assertEquals_("value in row " + i + " and col " + j + " should be empty", "",actualValues[i][j]);
+      } else {
+        // If within range of expected, the values need to match exactly
+        assertEquals_("value in row " + i + " and col " + j + " does not match", expected[i][j],actualValues[i][j]);
+      }
+    }
+  }
+
+  
+  assertEquals_("row numbers should match", expected.length, consecutiveFilledRows);
+
+  tearDown_();
+
+}
+function testAddCategoryAssertRangeFull() {
+
+  setUp_();
+
+
+  // The range must not chan
+  var expected =  testRangeFull.getValues();
+
+  // Add after the row we found. Adding + 1
+  addDataIntoRangeAtRowNumber_(testRangeFull, 6, [["a", "b", "c", "d", "e"]]);
+
+  var actualValues = testRangeFull.getValues();
+  var consecutiveFilledRows = 0;
+  for(var i = 0; i<actualValues.length;i++) {
+    if (actualValues[i][0] != "" || actualValues[i][1] != "" || actualValues[i][2] != "" || actualValues[i][3] != "") {
+      consecutiveFilledRows++;
+    }
+    for (var j = 0;j<actualValues[i].length;j++){ 
+      // Any cell values we cannot predict should be blank
+      if ( i >= expected.length ) {
+        assertEquals_("value in row " + i + " and col " + j + " should be empty", "",actualValues[i][j]);
+      } else {
+        // If within range of expected, the values need to match exactly
+        assertEquals_("value in row " + i + " and col " + j + " does not match", expected[i][j],actualValues[i][j]);
+      }
+    }
+  }
+
+  
+  assertEquals_("row numbers should match", expected.length, consecutiveFilledRows);
+
+  tearDown_();
+
+}
+
+
+
+function testGetLastRowIndexInDataRange() {
+
+    
+  setUp_();
+
+  
+  assertEquals_("Row count does not match", testRangeStartRow + testRangeValues.length -1, getLastRowInDataRange_(testRange).getRowIndex() );
+  assertEquals_("Row count does not match in the full range", testRangeStartRow + testRangeValues.length -1, getLastRowInDataRange_(testRangeFull).getRowIndex() );
+
+  tearDown_();
+
+}
+
+function testDeleteRowFromRangeByShiftingRowsUpAssertDeleteSuccess() {
+
+
+  setUp_();
+
+
+  var expected =  [
+    [ "B2", "C2", "D2","E2", "F2" ],
+    [ "B3", "C3", "D3","E3", "F3" ],
+    [ "B4", "C4", "D4","E4", "F4" ],
+    [ "B5", "C5", "D5","E5", "F5" ],
+  //  [ "B6", "C6", "D6","E6", "F6" ], // < deleted
+    [ "B7", "C7", "D7","E7", "F7" ],
+    [ "B8", "C8", "D8","E8", "F8" ],
+    [ "B9", "C9", "D9","E9", "F9" ],
+    [ "B10", "C10", "D10","E10", "F10" ],
+    [ "B11", "C11", "D11","E11", "F11" ],
+    [ "B12", "C12", "D12","E12", "F12" ],
+    [ "B13", "C13", "D13","E13", "F13" ],
+    [ "B14", "C14", "D14","E14", "F14" ],
+    [ "B15", "C15", "D15","E15", "F15" ],
+    [ "B16", "C16", "D16","E16", "F16" ],
+    [ "B17", "C17", "D17","E17", "F17" ],
+    [ "B18", "C18", "D18","E18", "F18" ],
+    [ "B19", "C19", "D19","E19", "F19" ],
+    [ "B20", "C20", "D20","E20", "F20" ],
+    [ "B21", "C21", "D21","E21", "F21" ],
+    [ "B22", "C22", "D22","E22", "F22" ],
+    [ "B23", "C23", "D23","E23", "F23" ],
+    [ "B24", "C24", "D24","E24", "F24" ],
+    [ "B25", "C25", "D25","E25", "F25" ],
+    [ "B26", "C26", "D26","E26", "F26" ],
+  ];
+
+  deleteRowFromRangeByShiftingRowsUp_(testRange, 6);
+
+  var actualValues = testRange.getValues();
+  var consecutiveFilledRows = 0;
+  for(var i = 0; i<actualValues.length;i++) {
+    if (actualValues[i][0] != "" || actualValues[i][1] != "" || actualValues[i][2] != "" || actualValues[i][3] != "") {
+      consecutiveFilledRows++;
+    }
+    for (var j = 0;j<actualValues[i].length;j++){ 
+      // Any cell values we cannot predict should be blank
+      if ( i >= expected.length ) {
+        assertEquals_("value in row " + i + " and col " + j + " should be empty", "",actualValues[i][j]);
+      } else {
+        // If within range of expected, the values need to match exactly
+        assertEquals_("value in row " + i + " and col " + j + " does not match", expected[i][j],actualValues[i][j]);
+      }
+    }
+  }
+
+  assertEquals_("row numbers should match", expected.length, consecutiveFilledRows);
+
+  tearDown_();
+
+}
+
+function testDeleteRowFromRangeByShiftingRowsUpAssertLastRowAndDeleteSuccess() {
+
+
+  setUp_();
+
+
+  var expected =  [
+    [ "B2", "C2", "D2","E2", "F2" ],
+    [ "B3", "C3", "D3","E3", "F3" ],
+    [ "B4", "C4", "D4","E4", "F4" ],
+    [ "B5", "C5", "D5","E5", "F5" ],
+    [ "B6", "C6", "D6","E6", "F6" ],
+    [ "B7", "C7", "D7","E7", "F7" ],
+    [ "B8", "C8", "D8","E8", "F8" ],
+    [ "B9", "C9", "D9","E9", "F9" ],
+    [ "B10", "C10", "D10","E10", "F10" ],
+    [ "B11", "C11", "D11","E11", "F11" ],
+    [ "B12", "C12", "D12","E12", "F12" ],
+    [ "B13", "C13", "D13","E13", "F13" ],
+    [ "B14", "C14", "D14","E14", "F14" ],
+    [ "B15", "C15", "D15","E15", "F15" ],
+    [ "B16", "C16", "D16","E16", "F16" ],
+    [ "B17", "C17", "D17","E17", "F17" ],
+    [ "B18", "C18", "D18","E18", "F18" ],
+    [ "B19", "C19", "D19","E19", "F19" ],
+    [ "B20", "C20", "D20","E20", "F20" ],
+    [ "B21", "C21", "D21","E21", "F21" ],
+    [ "B22", "C22", "D22","E22", "F22" ],
+    [ "B23", "C23", "D23","E23", "F23" ],
+    [ "B24", "C24", "D24","E24", "F24" ],
+    [ "B25", "C25", "D25","E25", "F25" ],
+    // [ "B26", "C26", "D26","E26", "F26" ], // < deleted
+  ];
+
+  var lastRowIndexWithDataInTestRange = testRange.getRowIndex() + testRangeValues.length;
+  deleteRowFromRangeByShiftingRowsUp_(testRange, lastRowIndexWithDataInTestRange);
+
+  var actualValues = testRange.getValues();
+  var consecutiveFilledRows = 0;
+  for(var i = 0; i<actualValues.length;i++) {
+    if (actualValues[i][0] != "" || actualValues[i][1] != "" || actualValues[i][2] != "" || actualValues[i][3] != "") {
+      consecutiveFilledRows++;
+    }
+    for (var j = 0;j<actualValues[i].length;j++){ 
+      // Any cell values we cannot predict should be blank
+      if ( i >= expected.length ) {
+        assertEquals_("value in row " + i + " and col " + j + " should be empty", "",actualValues[i][j]);
+      } else {
+        // If within range of expected, the values need to match exactly
+        assertEquals_("value in row " + i + " and col " + j + " does not match", expected[i][j],actualValues[i][j]);
+      }
+    }
+  }
+
+  assertEquals_("row numbers should match", expected.length, consecutiveFilledRows);
+
+  tearDown_();
+
+}
+
+
+function testFindCategoryAssertFound() {
+
+  
+  setUp_();
+
+  
+  assertEquals_("Item does not exist", false, findInRangeAtColumnIndex_(testRange, "E6", testRangeStartColumn) );
+  assertEquals_("Item does not exist", false, findInRangeAtColumnIndex_(testRange, "E6", testRangeStartColumn+1) );
+  assertEquals_("Item does not exist", false, findInRangeAtColumnIndex_(testRange, "E6", testRangeStartColumn+2) );
+  assertEquals_("Wrong line number", 6, findInRangeAtColumnIndex_(testRange, "E6", testRangeStartColumn+3) );
+  assertEquals_("Wrong line number (search is case insensitive)", 6, findInRangeAtColumnIndex_(testRange, "e6", testRangeStartColumn+3) );
+  assertEquals_("Item does not exist", false, findInRangeAtColumnIndex_(testRange, "E6", testRangeStartColumn+4) );
+
+
+  tearDown_();
+
+}
---
+++ Budget Tools.js
@@ -0,0 +1,352 @@
+function onOpen() {
+  SpreadsheetApp.getUi()
+    .createMenu('Budget Tools')
+    .addItem('Add category', 'addCategoryModal')
+    .addItem('Rename category', 'renameCategoryModal')
+    .addItem('Delete category', 'deleteCategoryModal')
+    .addToUi();
+}
+
+function addCategoryModal() {
+  var htmlDlg = HtmlService.createHtmlOutputFromFile('AddCategory.html')
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+
+  SpreadsheetApp.getUi()
+    .showModalDialog(htmlDlg, 'Add new category');
+};
+
+function deleteCategoryModal() {
+  var htmlDlg = HtmlService.createHtmlOutputFromFile('DeleteCategory.html')
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+
+  SpreadsheetApp.getUi()
+    .showModalDialog(htmlDlg, 'Delete category');
+};
+
+function renameCategoryModal() {
+  var htmlDlg = HtmlService.createHtmlOutputFromFile('RenameCategory.html')
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+
+  SpreadsheetApp.getUi()
+    .showModalDialog(htmlDlg, 'Rename category');
+};
+
+
+
+
+function getCategoriesTypesList() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var categoryIcon = spreadsheet.getRangeByName("v_ReportableCategorySymbol").getValue();
+  var nonReportableBudgetCategoryIcon = spreadsheet.getRangeByName("v_NonReportableCategorySymbol").getValue();
+  var debtCategoryIcon = spreadsheet.getRangeByName("v_CategoryGroupSymbol").getValue();
+  var categoryGroupIcon = spreadsheet.getRangeByName("v_DebtAccountSymbol").getValue();
+
+  var categoryTypeList = [
+    [
+      categoryIcon, categoryIcon + " Category",
+    ],
+    [
+      nonReportableBudgetCategoryIcon, nonReportableBudgetCategoryIcon + " Non-reportable Category",
+    ],
+    [
+      debtCategoryIcon, debtCategoryIcon + " Debt Category",
+    ],
+    [
+      categoryGroupIcon, categoryGroupIcon + " Category Group",
+    ]
+  ];
+
+  console.info(categoryTypeList);
+
+  return categoryTypeList;
+}
+
+function getCategoriesList() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var configData = spreadsheet.getRangeByName("r_ConfigurationData");
+  var categoryList = [];
+  configData.getValues().forEach(function (val) {
+    if (val[1] != "") {
+      categoryList.push([val[1], val[0] + " " + val[1]]);
+    }
+  })
+  console.info(categoryList);
+
+  return categoryList;
+}
+
+function getEmergencyFundTypesList() {
+  return [
+    ["✓", "✓ Include in Calculation"],
+    ["✕", "✕ Exclude from Calculation"]
+  ];
+}
+
+
+function handleRenameCategory(form) {
+  var spreadsheet = SpreadsheetApp.getActive();
+  
+
+  var oldCategoryName = form.CategoryName;
+  var newCategoryName = form.NewCategoryName;
+
+  if ( oldCategoryName == null || oldCategoryName.length == 0 ) {
+    Logger.log("ERROR - category name is invalid: '%s'", oldCategoryName);
+    return;
+  }
+
+  if ( newCategoryName == null || newCategoryName.length == 0 ) {
+    Logger.log("ERROR - category name is invalid: '%s'", newCategoryName);
+    return;
+  }
+
+  var configData = spreadsheet.getRangeByName("r_ConfigurationData");
+  if (findInRangeAtColumnIndex_(configData, newCategoryName, getConfigDataCategoryNameColumnIndex_() ) != false ) {
+    Logger.log("ERROR - Category '%s' already exists on the configuration sheet.", oldCategoryName);
+    return;
+  }
+
+  // rename in config sheet
+  findReplaceInRange_(configData, oldCategoryName, newCategoryName);
+
+  // rename in transactions
+  var categoriesRangeInTransactionSheet = spreadsheet.getRangeByName("trx_Categories");
+  findReplaceInRange_(categoriesRangeInTransactionSheet, oldCategoryName, newCategoryName);
+
+  //rename in category transfers
+  var fromCategoriesRangeInCategoryTransferSheet = spreadsheet.getRangeByName("cts_FromCategories");
+  var toCategoriesRangeInCategoryTransferSheet = spreadsheet.getRangeByName("cts_ToCategories");
+  findReplaceInRange_(fromCategoriesRangeInCategoryTransferSheet, oldCategoryName, newCategoryName);
+  findReplaceInRange_(toCategoriesRangeInCategoryTransferSheet, oldCategoryName, newCategoryName);
+
+  // Rename in reports
+  Logger.log("Changing reference from '%s' to '%s' from Transactions Report sheet.", oldCategoryName, newCategoryName);
+  findReplaceInRange_(getTransactionsReportCategoryFilter_(), oldCategoryName,  newCategoryName);
+  Logger.log("Changing reference from '%s' to '%s' from Category Transfer sheet.", oldCategoryName, newCategoryName);
+  findReplaceInRange_(getCategoryTransferCategoryFilter_(), oldCategoryName,  newCategoryName);
+  Logger.log("Changing reference from '%s' to '%s' from Category Report sheet.", oldCategoryName, newCategoryName);
+  findReplaceInRange_(getCategoryReportCategoryFilter_(), oldCategoryName,  newCategoryName);
+  Logger.log("Changing reference from '%s' to '%s' from Spending Report sheet.", oldCategoryName, newCategoryName);
+  findReplaceInRange_(getSpendingReportsCategoryFilter_(), oldCategoryName,  newCategoryName);
+  Logger.log("Changing reference from '%s' to '%s' from Trend Report sheet.", oldCategoryName, newCategoryName);
+  getTrendReportsCategoryFilters_().getRanges().forEach(range => range.createTextFinder(oldCategoryName).replaceAllWith( newCategoryName));
+
+
+}
+
+
+
+
+
+// Handle Add Category form submit
+function handleAddCategory(form) {
+
+  var insertAfter = form.AddAfter;
+  var newCategorySymbol = form.NewCategorySymbol;
+  var newCategoryName = form.NewCategoryName;
+  var newCategoryMonthlyAmount = form.NewCategoryMonthlyAmount;
+  var newCategoryAmount = form.NewCategoryAmount;
+  var newEmergencyFund = form.NewEmergencyFund;;
+  var toInsert = [[newCategorySymbol, newCategoryName, newCategoryAmount, newCategoryMonthlyAmount, newEmergencyFund]];
+
+  var spreadsheet = SpreadsheetApp.getActive();
+  var configData = spreadsheet.getRangeByName("r_ConfigurationData");
+  var configSheet = configData.getSheet();
+  var configDataFirstRow = configData.getRowIndex();
+  var configDataLastRow = configData.getLastRow();
+
+  if (configSheet.getRange(configDataLastRow, getConfigDataCategoryNameColumnIndex_()).getValue() != "") {
+    Logger.log("Error - the spreadsheet is full. Please clear some categories from the configuration sheet")
+    return;
+  }
+
+  var insertAtRow = findInRangeAtColumnIndex_(configData, insertAfter, getConfigDataCategoryNameColumnIndex_());
+
+  if (insertAtRow == false) {
+    insertAtRow = configDataFirstRow;
+  }
+
+  // Add after the line we found. Adding 1
+  addDataIntoRangeAtRowNumber_(configData, insertAtRow + 1, toInsert);
+
+  return true;
+}
+
+
+
+
+// Handle Delete Category form submit
+function handleDeleteCategory(form) {    // Select Sheet   var ss = SpreadsheetApp.getActiveSpreadsheet();
+
+  var categoryName = form.DeleteCategory;
+
+  if (categoryName == null || categoryName.length == 0) {
+    Logger.log("Error - no category name provided.");
+    return;
+  }
+
+  var spreadsheet = SpreadsheetApp.getActive();
+  var configData = spreadsheet.getRangeByName("r_ConfigurationData");
+
+  // Find the last row within hidden categories that is empty.
+  var lastDataCellInRange = getLastRowInDataRange_(getConfigDataHiddenCategoriesRange_());
+
+  // Enure Hidden Categories range isn't full
+  if (lastDataCellInRange.getRowIndex() == getConfigDataHiddenCategoriesRange_().getLastRow()) {
+    Logger.log("Error - the hidden categories range is full. Please clear some categories from the hidden categories range on the configuration sheet");
+    return;
+  }
+  // Determine which row index needs to be removed.
+  var rowIndexToRemove = findInRangeAtColumnIndex_(configData, categoryName, getConfigDataCategoryNameColumnIndex_());
+
+  // Do nothing if the category cannot be found
+  if (rowIndexToRemove == false) {
+    Logger.log("Error - the category '%s' could not be found in the configuration sheet.", categoryName);
+    return false;
+  }
+
+  // Add after the line we found. Adding 1
+  var toInsert = [[categoryName]];
+  Logger.log("Adding category '%s' to list of hidden categories", categoryName);
+  addDataIntoRangeAtRowNumber_(getConfigDataHiddenCategoriesRange_(), lastDataCellInRange.getRow() + 1, toInsert);
+
+  // Delete the category from the config range by copying all following rows one line up.
+  Logger.log("Deleting category '%s' from list of categories on config sheet.", categoryName);
+  deleteRowFromRangeByShiftingRowsUp_(configData, rowIndexToRemove);
+
+  // Clear any of the report filters when deleting/
+
+  Logger.log("Clear reference to '%s' from Transactions Report sheet.", categoryName);
+  findReplaceInRange_(getTransactionsReportCategoryFilter_(), categoryName, "");
+  Logger.log("Clear reference to '%s' from Category Transfer sheet.", categoryName);
+  findReplaceInRange_(getCategoryTransferCategoryFilter_(), categoryName, "");
+  Logger.log("Clear reference to '%s' from Category Report sheet.", categoryName);
+  findReplaceInRange_(getCategoryReportCategoryFilter_(), categoryName, "");
+  Logger.log("Clear reference to '%s' from Spending Report sheet.", categoryName);
+  findReplaceInRange_(getSpendingReportsCategoryFilter_(), categoryName, "");
+  Logger.log("Clear reference to '%s' from Trend Report sheet.", categoryName);
+  getTrendReportsCategoryFilters_().getRanges().forEach(range => range.createTextFinder(categoryName).replaceAllWith(""));
+
+  return true;
+}
+
+/**
+ * Helper functions below
+ */
+
+function getTransactionsReportCategoryFilter_() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var transactionsData = spreadsheet.getRangeByName("trx_Accounts");
+  var transactionsSheet = transactionsData.getSheet();
+  return transactionsSheet.getRange("F4");
+}
+function getCategoryTransferCategoryFilter_() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var categoryTransfersData = spreadsheet.getRangeByName("cts_Dates");
+  var categoryTransfersSheet = categoryTransfersData.getSheet();
+  return categoryTransfersSheet.getRange("E3");
+}
+function getCategoryReportCategoryFilter_() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var categoryReportSheet = spreadsheet.getSheetByName("Category Reports");
+  return categoryReportSheet.getRange("B7");
+}
+function getSpendingReportsCategoryFilter_() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var categoryReportSheet = spreadsheet.getSheetByName("Spending Reports");
+  return categoryReportSheet.getRange("B36:C36");
+}
+function getTrendReportsCategoryFilters_() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var categoryReportSheet = spreadsheet.getSheetByName("Trend Reports");
+  return categoryReportSheet.getRangeList(["B8", "B10", "B12", "B14", "B16", "B18", "B28:C28"]);
+}
+
+function getLastRowInDataRange_(range) {
+  var lastDataRowIndex = range.getNextDataCell( SpreadsheetApp.Direction.DOWN).getRowIndex();
+
+  //Ensure we don't exceed the range itself.
+  if (lastDataRowIndex > range.getLastRow()) {
+    lastDataRowIndex = range.getLastRow();
+  }
+  return range.getSheet().getRange( lastDataRowIndex, range.getColumn(), 1, range.getNumColumns());
+}
+
+function getConfigDataCategoryNameColumnIndex_() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var configData = spreadsheet.getRangeByName("r_ConfigurationData");
+  var configDataFirstColumn = configData.getColumn();
+  return configDataFirstColumn + 1;
+}
+
+// This is a work-around until a named range for the hidden categories exists
+function getConfigDataHiddenCategoriesRange_() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  var configData = spreadsheet.getRangeByName("r_ConfigurationData");
+  var configSheet = configData.getSheet();
+  return configSheet.getRange("H42:H86");
+}
+
+
+function deleteRowFromRangeByShiftingRowsUp_(range, rowIndex) {
+  var lastDataRowInRange = getLastRowInDataRange_(range);
+  // Only shift items up if this isn't the last row.
+  if (lastDataRowInRange.getRowIndex() > rowIndex) {
+    Logger.log("Not deleting last row in range...Shifting rows up by 1");
+    range
+      .getSheet()
+      .getRange(rowIndex + 1, range.getColumn(), range.getLastRow() - rowIndex, range.getNumColumns())
+      .copyTo(range.getSheet().getRange(rowIndex, range.getColumn()));
+  }
+  Logger.log("Clearing the last item in the list.");
+  lastDataRowInRange.clear();
+  return;
+  // range.getSheet().getRange( rowToClear, range.getNumColumns(),1,range.getNumColumns()).setValue("");
+}
+function addDataIntoRangeAtRowNumber_(range, rowNumber, toInsert) {
+  var sheet = range.getSheet();
+  var rangeFirstColumn = range.getColumn();
+  var rangeLastRow = range.getLastRow();
+
+  // check that there is space for the row to be added.
+  var lastRowValue = sheet.getRange(rangeLastRow,rangeFirstColumn).getValue();
+  if (lastRowValue != "") {
+    Logger.log("addDataIntoRangeAtRowNumber rowNumber:" + rowNumber + " failed. The range is full.");
+    return;
+  }
+
+  // copy all rows after the row we found down one row
+  var numRowsToCopy = rangeLastRow - rowNumber ;
+  sheet.getRange(rowNumber, rangeFirstColumn, numRowsToCopy, toInsert[0].length).copyTo(
+    sheet.getRange(rowNumber + 1, rangeFirstColumn, numRowsToCopy, toInsert[0].length),
+    { contentsOnly: true }
+  );
+
+
+  // Insert new row under the one we found
+  sheet.getRange(rowNumber, rangeFirstColumn, 1, toInsert[0].length).setValues(toInsert);
+}
+
+function findReplaceInRange_(range, find, replace) {
+  range
+    .createTextFinder(find)
+    .replaceAllWith(replace);
+}
+
+function findInRange_(range, needle) {
+  var firstMatch = range.createTextFinder(needle).findNext();
+
+  if (firstMatch != null) {
+    return firstMatch.getRowIndex();
+  }
+  return false;
+}
+function findInRangeAtColumnIndex_(range, needle, columnIndex) {
+  var rangeToSearch = range.getSheet().getRange(range.getRowIndex(), columnIndex, range.getNumRows(), 1);
+
+  return findInRange_(rangeToSearch, needle );
+
+}
